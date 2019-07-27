e72e4b8af6748860f83e545dc35f17b6ed370b2f
nargok <nargok@gmail.com>
2019-07-27 22:37:58 +0900

    funciton with args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     println!("hello world");
 
-    another_function();
+    another_function(5);
 }
 
-fn another_function() {
-    println!("Another function");
+fn another_function(x: i32) {
+    println!("The value of x is: {}", x);
 }
 
 
